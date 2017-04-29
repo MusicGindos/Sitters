8a608c3dc95aa3eb14f5c7e6627b8f174cd3e0fb
DassiShenkar <dassi.shenkar@gmail.com>
2017-04-29 14:15:37 +0300

    changed id to _id

--- modules/mongoose/index.js
+++ modules/mongoose/index.js
@@ -117,7 +117,7 @@ exports.getMatches = (req, res) => {
 };
 
 exports.getParent = (req, res) => {
-    Parent.findOne().where('_id', req.body.id).exec(function (err, doc) {
+    Parent.findOne().where('_id', req.body._id).exec(function (err, doc) {
         if (err) {
             error(res, err);
         }
