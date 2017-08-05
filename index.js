e3f389db7bbb3e7285aa9b62a076f7c81fa62a4b
DassiShenkar <dassi.shenkar@gmail.com>
2017-08-05 19:20:15 +0300

    refactor updateUser

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
-var express		= require('express'),
-    controller 	= require('./server'),
-    cors 		= require('cors'),
-    bodyParser 	= require('body-parser'),
-    port 		= process.env.PORT || 4444,
-    app 		= express();
+
+const express = require('express'),
+    controller = require('./server'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 4444,
+    app = express();
 
 //server config
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.use(express.static(__dirname + '/build/'));
 app.use(cors());
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader("Content-Type", "text/html");
@@ -21,33 +22,33 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/', controller.index);
+// app.get('/', controller.index);
 
-app.post('/parent/create', controller.createParent);
-app.post('/parent/update', controller.updateParent);
-app.delete('/parent/delete', controller.deleteParent);
-app.post('/parent/get', controller.getParent);
-app.post('/parent/getMatches', controller.getMatches);
-app.post('/parent/getSitters', controller.getSitters);
-app.post('/parent/updateMutualFriends', controller.updateMutualFriends);
-app.post('/parent/notifications', controller.notifications);
-app.post('/sitter/create', controller.createSitter);
-app.post('/sitter/update', controller.updateSitter);
-app.delete('/sitter/delete', controller.deleteSitter);
-app.post('/sitter/get', controller.getSitter);
-app.post('/sitter/updateMutualFriends', controller.updateMutualFriends);
-app.post('/user/getUser', controller.getUser);
+app.post('/user/get', controller.getUser);
+app.post('/user/create', controller.createUser);
+app.post('/user/update', controller.updateUser);
+// app.delete('/user/delete', controller.deleteUser);
+// app.post('/user/updateFriends', controller.updateFriends);
 app.post('/invite/create', controller.sendInvite);
-app.post('/invite/updateInvite', controller.updateInvite);
+app.post('/invite/update', controller.updateInvite);
+// app.post('/invite/updateInvite', controller.updateInvite);
+// app.post('/parent/getMatches', controller.getMatches);
+// app.post('/parent/getSitters', controller.getSitters);
+// app.post('/parent/updateMutualFriends', controller.updateMutualFriends);
+// app.delete('/parent/delete', controller.deleteParent);
+// app.post('/parent/notifications', controller.notifications);
+// app.delete('/sitter/delete', controller.deleteSitter);
+// app.post('/sitter/updateMutualFriends', controller.updateMutualFriends);
 app.get('*', (req,res,next) => {
     let err = new Error();
-    err.status = 404;
+    err.status = 400;
+    err.message = "Bad Request";
     next(err);
 });
 
 app.use((err,req,res) => {
-    if(err.status == 404){
-    return res.status(404).end(err.message);
+    if(err.status == 400){
+    return res.status(400).end(err.message);
     }
     else if(err.status == 500){
         return res.status(500).end(err.message);
